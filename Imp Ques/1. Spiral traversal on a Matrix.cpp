7bca6d936c32905c301b8202950e3ac35f421d72
Sushil Kumar Verma <64746160+skv06@users.noreply.github.com>
2021-07-18 14:41:14 +0530

    Update 1. Spiral traversal on a Matrix.cpp

--- Imp Ques/1. Spiral traversal on a Matrix.cpp
+++ Imp Ques/1. Spiral traversal on a Matrix.cpp
@@ -2,11 +2,11 @@
 //  Spiral traversal on a matrix
 
 //  Input:   r = 4, c = 4
-             matrix[][] = {{1, 2, 3, 4},
+/*             matrix[][] = {{1, 2, 3, 4},
                           {5, 6, 7, 8},
                           {9, 10, 11, 12},
                           {13, 14, 15,16}}
-
+*/
 //  Output:   1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
 
 #include <bits/stdc++.h> 
